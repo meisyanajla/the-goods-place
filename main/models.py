6fdc0d09673bb290a84fdca75a8992d919e89281
meisyanajla <meisyaanajlaa@gmail.com>
2024-09-18 02:38:47 +0700

    Redesign UI

--- main/models.py
+++ main/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import uuid
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     quantity = models.IntegerField()
     rating = models.DecimalField(max_digits=3, decimal_places=2)
+    
